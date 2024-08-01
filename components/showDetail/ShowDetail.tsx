bb5cade3f13b9887d777436971b91883c3675270
tibetdev <tenzinkunchok2022@gmail.com>
2024-08-01 13:39:14 +0200

    Add ShowDetail component for displaying show details and episodes

---
+++ components/showDetail/ShowDetail.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+import styles from './ShowDetail.module.scss';
+import Link from 'next/link';
+
+type Episodes = [{ id: number; name: string; season: number }];
+
+type Show = {
+  name: string;
+  summary: string;
+  image: {
+    original: string;
+  };
+};
+
+const ShowDetail: React.FC<{ show: Show; episodes: Episodes }> = ({
+  show,
+  episodes,
+}) => {
+  // console.log(episodes);
+  return (
+    <>
+      <h1>{show.name}</h1>
+      <p>{show.summary}</p>
+      <img src={show.image.original} alt={show.name} />
+      <ul>
+        {episodes.map((episode) => (
+          <li key={episode.id}>
+            <Link href={`/episode/${episode.id}`}>
+              {episode.name} - Season {episode.season}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default ShowDetail;
