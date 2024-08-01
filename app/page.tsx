8067215b51b7cb3874c56cafb2f4a8165a3581db
tibetdev <tenzinkunchok2022@gmail.com>
2024-08-01 21:37:39 +0200

    Update welcome page layout and styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,22 @@
-import styles from './page.module.scss';
-import Link from 'next/link';
+import classes from './page.module.scss';
+import powerpuff from '@/public/powerpuffGirls.png';
+import LinkButton from '@/components/button/LinkButton';
+import HomeImage from '@/components/homeImage/HomeImage';
 
 export default function Home() {
+  // This is a hardcoded showId for the powerpuff girls show on TVMaze
+  // You can replace this with any showId you want to display
+  const id = 6771;
+
   return (
     <main>
-      <div className={styles.container}>
-        <h1> Welcome to the PowerPuff Girls Show</h1>
-        <Link href='/show/6771'>Go to the show Details</Link>
+      <div className={classes.welcome}>
+        <HomeImage
+          imgLink={powerpuff}
+          desc='Powerpuff girls'
+          url={`/show/${id}`}
+        />
+        <LinkButton text={`Let's Fly`} url={`/show/${id}`} />
       </div>
     </main>
   );
