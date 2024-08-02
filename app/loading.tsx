745c65ad88580f7ef5a28d97fc6fa0176a18a5a4
tibetdev <tenzinkunchok2022@gmail.com>
2024-08-02 17:20:33 +0200

    Updated loading page -added smooth scroll

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,10 @@
 import classes from './loading.module.scss';
 
 export default function Loading() {
-  return <p className={classes.loading}>Loading...</p>;
+  return (
+    <main className='loading'>
+      <h1 className='loading__header'>Please be patient.</h1>
+      <p className='loading__message'>Loading ...</p>
+    </main>
+  );
 }
