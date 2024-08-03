53c8258c762e641d010acf4cae368055c9fa8cf6
tibetdev <tenzinkunchok2022@gmail.com>
2024-08-04 00:21:32 +0200

    refactor: Update API request functions with error handling

--- api/api.ts
+++ api/api.ts
@@ -2,26 +2,6 @@ import axios from 'axios';
 
 const API_URL = 'https://api.tvmaze.com';
 
-export const fetchShowDetails = async (showId: number) => {
-  try {
-    const response = await axios.get(
-      `${API_URL}/shows/${showId}?embed=episodes`
-    );
-    return response.data;
-  } catch (error) {
-    throw new Error('404');
-  }
-};
-
-export const fetchShowEpisodes = async (showId: number) => {
-  try {
-    const response = await axios.get(`${API_URL}/shows/${showId}/episodes`);
-    return response.data;
-  } catch (error) {
-    throw new Error('404');
-  }
-};
-
 export const fetchEpisodeDetails = async (episodeId: number) => {
   try {
     const response = await axios.get(`${API_URL}/episodes/${episodeId}`);
